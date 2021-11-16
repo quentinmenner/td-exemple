20dcbad9b695a11316727c86e8933a202b60a09a
Quentin Menner <82613122+quentinmenner@users.noreply.github.com>
2021-11-16 13:58:47 +0100

    Add addition.py and untitled.ipynb

---
+++ addition.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+def addition(x,y):
+    z=x+y
+    return z
+
+a=int(input('Première variable : '))
+b=int(input('Deuxième variable : '))
+result=print("Total : ", addition(a,b))
